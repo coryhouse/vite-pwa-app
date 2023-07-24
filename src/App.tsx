3fe3442e7e0c8ea5c75d3733c42abbcd6371070e
Cory House <housecor@gmail.com>
2023-07-23 19:12:46 -0500

    add demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { StrictMode } from "react";
 import "./App.css";
 
+import { useRegisterSW } from "virtual:pwa-register/react";
+
+const intervalMS = 10 * 1000;
+
 function App() {
-  const [count, setCount] = useState(0);
+  useRegisterSW({
+    onRegistered(r) {
+      r &&
+        setInterval(() => {
+          console.log("calling update?");
+          r.update();
+        }, intervalMS);
+    },
+  });
 
   return (
-    <>
-      <div>
-        <h1>Deployment date: {new Date().toUTCString()}</h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <StrictMode>
+      <h1>Loaded: {new Date().toUTCString()}</h1>
+    </StrictMode>
   );
 }
 
